f688f14808fb479f6c45ca2fc18ae4c4e8eb7169
diogograve90 <61422201+diogograve90@users.noreply.github.com>
2020-05-22 20:55:03 +0100

    Sixth test
    In the previous commit, I added the generation of animals in the generator class and I tested the generation in the main class.

--- src/VetClinic/VetClinic.java
+++ src/VetClinic/VetClinic.java
@@ -8,45 +8,45 @@ import VetClinic.util.Generator;
 
 public class VetClinic {
 
-	ArrayList<Staff> adminStaff = Generator.adminGenerator();
-	ArrayList<Staff> medicalStaff = Generator.medicalGenerator();
-	ArrayList<Animal> animals = Generator.animalGenerator();
+	public static void main(String[] args) {
 
+		ArrayList<Staff> adminStaff = Generator.adminGenerator();
+		ArrayList<Staff> medicalStaff = Generator.medicalGenerator();
+		ArrayList<Animal> animals = Generator.animalGenerator();
+		int i = 0;
+		int j = 0;
+		int k = 0;
 
+		for (Staff as : adminStaff) {
+			
+			i++;
+			System.out.println(i + " " + as.toString());
 
+		}
 
-	for (Staff as : adminStaff) {
-		
-		int i;
-		i++;
-		System.out.println(i + " " + as.toString());
-
-	}
-
-	for (int s1 = 0; s < 10; s++) {
-		System.out.println("");
-	}
-	
-	for(Staff ms : medicalStaff) {
-		
-		int j;
-		j++;
-		System.out.println(j+" "+ms.toString());
-		
-	}
-	
-	for (int s = 0; s < 10; s++) {
-		System.out.println("");
-	}
-	
-	for(Animal an : animals ) {
-		
-		int k;
-		k++;
-		System.out.println(k+" "+an.toString());
-		
+		for (int s = 0; s < 10; s++) {
+			System.out.println("");
 		}
 		
+		for(Staff ms : medicalStaff) {
+			
+			j++;
+			System.out.println(j+" "+ms.toString());
+			
+		}
+		
+		for (int s = 0; s < 10; s++) {
+			System.out.println("");
+		}
+		
+		for(Animal an : animals ) {
+			
+			k++;
+			System.out.println(k+" "+an.toString());
+			
+		}
+		
+
 	}
-	
+
 }
